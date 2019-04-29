856c9427e848fd3a6e2c1f0129d06a25476a6361
lcfarm_cy <chenyun@nongfadai.com>
2019-04-29 11:17:59 +0800

    修改只调 android 原生

--- lib/flutter_status_bar.dart
+++ lib/flutter_status_bar.dart
@@ -6,16 +6,17 @@ class FlutterStatusBar {
   static const MethodChannel _channel =
       const MethodChannel('flutter_status_bar');
 
+  ///设置 android 状态栏透明，IOS flutter 已内置，直接设置 appbar [brightness] [backgroundColor]
   static void setTranslucent() {
-    if(Platform.isAndroid){
+    if (Platform.isAndroid) {
       _channel.invokeMethod('setTransparent');
     }
   }
 
+  ///设置 android 状态栏图标，IOS flutter 已内置，直接设置 appbar [brightness] [backgroundColor]
   static void setLightStatusBar() {
-    if(Platform.isAndroid){
+    if (Platform.isAndroid) {
       _channel.invokeMethod('setLightStatusBar');
     }
-
   }
 }
